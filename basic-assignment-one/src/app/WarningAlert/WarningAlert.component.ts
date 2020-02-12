9fda29c5d6bb73df2a8eefbe9b47cf35f830e829
lukeskywalkerthethird <bnyagah90@gmail.com>
2020-02-13 00:30:05 +0300

    commit this

---
+++ basic-assignment-one/src/app/WarningAlert/WarningAlert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-WarningAlert',
+    templateUrl:'./WarningAlert.component.html'
+})
+
+export class WarningAlertComponent{
+    
+}
